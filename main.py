a3320b72bed068ea2f84b95b777fc73f00cb2fbf
Linh Tran <lmt2203@cumc.columbia.edu>
2021-06-13 16:56:25 -0700

    importing local scripts

--- main.py
+++ main.py
@@ -40,3 +40,4 @@ to {} if you submit all assignments before the due date.\n\n"
 # write a for loop that iterates through each set of names, assignments, and grades to print each student's message
 for name, assignment, grade in zip(names, assignments, grades):
     print(message.format(name, assignment, grade))
+
