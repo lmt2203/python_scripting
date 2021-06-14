98e7e4099fc67af4982fd9a88de3ae6ea1e3ca29
Linh Tran <lmt2203@cumc.columbia.edu>
2021-06-14 15:14:56 -0700

    data types and data structures

---
+++ scripting.py
@@ -0,0 +1,16 @@
+# Strings
+
+string_1 = "this is a string"
+
+print(string_1)
+
+first_word = 'Hello'
+second_word = 'There'
+print(first_word + ' ' + second_word)
+print(first_word * 5)
+print(len(first_word))   #integer
+
+## index into strings
+print(first_word[3])
+
+# Data types: int, float, string, bool
